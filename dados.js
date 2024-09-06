52ed2d9dc336a65eacc4b68f8ebe6616e3a27074
LeonardoFernandesRibeiro <leo.fribeiro@hotmail.com>
2024-09-06 11:05:15 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo : "Canva",
+        descricao: "O Canva é uma plataforma online de design gráfico que revolucionou a forma como criamos materiais visuais. Seja você um profissional de marketing, um estudante, um pequeno empresário ou apenas alguém que busca criar designs personalizados, o Canva oferece uma interface intuitiva e ferramentas poderosas para dar vida às suas ideias.",
+        link: "https://www.canva.com/",
+        tags: "can c art arte des mark"
+    },
+    {
+        titulo: "Adobe Illustrator",
+        descricao: "O Adobe Illustrator é um software líder de mercado, desenvolvido pela Adobe, especializado na criação e edição de gráficos vetoriais. Essa ferramenta é indispensável para designers gráficos, ilustradores e profissionais de diversas áreas que necessitam de alta precisão e escalabilidade em seus trabalhos.",
+        link: "https://www.adobe.com/br/products/illustrator/campaign/pricing.html?gclid=CjwKCAjwreW2BhBhEiwAavLwfFVofLUAYSM08TZelPvOtaxKIGimm4qOs5YFw9AOGi-woRMmCAGHWBoCiKwQAvD_BwE&sdid=&mv=search&mv2=paidsearch&ef_id=CjwKCAjwreW2BhBhEiwAavLwfFVofLUAYSM08TZelPvOtaxKIGimm4qOs5YFw9AOGi-woRMmCAGHWBoCiKwQAvD_BwE:G:s&s_kwcid=AL!3085!3!470955336927!e!!g!!adobe%20illustrator!188190582!110348310163&gad_source=1",
+        tags: "ado a il art arte des mark"
+      },
+      {
+        titulo: "CorelDRAW",
+        descricao: "O CorelDRAW é um software de design gráfico vetorial amplamente utilizado por profissionais e amadores ao redor do mundo. Ele oferece uma ampla gama de ferramentas para criar e editar gráficos vetoriais, ilustrações, layouts de página e muito mais.",
+        link: "https://www.coreldraw.com/br/product/coreldraw/?x-vehicle=ppc_brkws&utm_medium=cpc&utm_source=google&utm_campaign=&utm_term=corel%20draw&utm_content=&utm_id=11287101014&extensionid=&matchtype=e&device=c&devicemodel=&creative=631199132118&network=g&placement=&x-source=ppc&x-target=ppc&promo=ppc&gad_source=1&gclid=CjwKCAjwreW2BhBhEiwAavLwfHPnlIMux69u8dv_UGSQeW3--ZuZmfeFJ7D_qlbAOBCsVhyN4LxJ3RoC-2wQAvD_BwE",
+        tags: "cor c dr art arte des mark"
+      },
+      {
+        titulo: "Adobe Photoshop",
+        descricao: "O Adobe Photoshop é um software líder de mercado, amplamente utilizado por profissionais e entusiastas da área de design gráfico e edição de imagens. Com uma vasta gama de ferramentas e recursos avançados, o Photoshop permite manipular imagens digitais de maneira precisa e criativa.",
+        link: "https://www.adobe.com/br/products/photoshop.html?promoid=HZG8WST6&mv=other",
+        tags: "ado a p phot art arte des mark"
+      },
+    ];
+
